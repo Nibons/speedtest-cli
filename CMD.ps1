f993dc6c61063bc55039a0c9817c3fe63ed30c25
Clay Haertzen <chaertzen@deloitte.com>
2017-11-12 20:27:10 -0500

    fixed issue with CMD file

--- CMD.ps1
+++ CMD.ps1
@@ -1,7 +1,11 @@
-param([string]$logFileName)
+param([string]$logFileName = 'speedtest.csv')
 
 $file_logfile = "c:/Logs/$logFileName"
 
+if(!(test-path $file_logfile)) {
+    New-Item -Path $file_logfile -ItemType File
+}
+
 do{
-    speedtest-cli --json  | ConvertFrom-Json | tee-object -FilePath $file_logfile -Append | format-table
+    speedtest-cli --json | tee-object -FilePath $file_logfile -Append  | ConvertFrom-Json | format-table
 }while($true)
